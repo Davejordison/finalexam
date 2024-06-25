80e3df79592c2dd889be883aa57cc1a9a13e8388
Davejordison <janny.dave.1987@gmail.com>
2024-06-25 10:24:00 +0200

    coding

--- coding/index.js
+++ coding/index.js
@@ -13,7 +13,26 @@ Each object should have:
 value: the common element.
 indexSum: the sum of the indices of this element in both arrays.
 */
-export function twistedArrayIntersection(arr1, arr2) {}
+export function twistedArrayIntersection(arr1, arr2) {
+    const result = [];
+
+    for (let i = 0; i < arr1.length; i++) {
+        for (let j = 0; j < arr2.length; j++) {
+            if (arr1[i] === arr2[j]) {
+                result.push({
+                    value: arr1[i],
+                    indexSum: i + j
+                });
+                break;
+            }
+        }
+    }
+    
+    return result;
+}
+
+
+
 
 
 /*
@@ -32,4 +51,21 @@ The initial balance is 0.
 If a withdrawal causes the balance to go below zero, the transaction should be ignored,
 and the function should continue processing the remaining transactions.
 */
-export function processTransactions(transactions) {}
+export function processTransactions(transactions) {
+    let balance = 0;
+
+  for (const transaction of transactions) {
+    const { type, amount } = transaction;
+
+    if (type === "deposit") {
+      balance += amount;
+    } else if (type === "withdrawal") {
+      if (balance - amount >= 0) {
+        balance -= amount;
+      }
+    }
+  }
+
+  return balance;
+}
+
